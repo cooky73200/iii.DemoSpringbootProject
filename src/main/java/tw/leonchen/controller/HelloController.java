d5f663737bc184d769b7801d73bd9a88e4b4e92d
OKder <cooky73200@gmail.com>
2021-09-07 10:36:18 +0800

    add HelloController.java

---
+++ src/main/java/tw/leonchen/controller/HelloController.java
@@ -0,0 +1,15 @@
+package tw.leonchen.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //相當於@Controller + @ResponseBody
+public class HelloController {
+
+	@GetMapping(path="/hello.controller")
+	public String processAction() {
+		return "Hello mary";
+
+	}
+
+}
