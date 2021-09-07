e56f6071220b6526492fd1f18389255e913c70fa
OKder <cooky73200@gmail.com>
2021-09-07 14:14:20 +0800

    設定靜態資源路徑前

--- src/main/java/tw/leonchen/DemoSpringbootProjectApplication.java
+++ src/main/java/tw/leonchen/DemoSpringbootProjectApplication.java
@@ -2,8 +2,10 @@ package tw.leonchen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan //會自動尋找當前包下的annotation
 public class DemoSpringbootProjectApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/tw/leonchen/controller/PathVariableController.java
@@ -0,0 +1,17 @@
+package tw.leonchen.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PathVariableController {
+
+	@GetMapping("/member/{mid}")
+	public String processPathAction(@PathVariable("mid")String memberId) {
+		return "memberId: " + memberId;
+
+	}
+
+
+}
